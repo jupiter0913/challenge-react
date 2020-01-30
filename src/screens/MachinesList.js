62ae39482219d842b5bc737be30ed219463c1c26
jupiter0913 <jupiter0913@protonmail.com>
2020-01-30 20:25:05 +0300

    [add] Add button component in project.

--- src/screens/MachinesList.js
+++ src/screens/MachinesList.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import Button from '../components/Button';
+import PropTypes from "prop-types";
 
 const objectStyle = {
   float: 'left',
   marginRight: 30,
   width: 100,
+  height: 30
 };
 
 export default class MachinesList extends Component {
+  static propTypes = {
+    clickHandler: PropTypes.func,
+  };
+
+  handleClick = id => {
+    console.log(id);
+  };
   render() {
     return (
       <div style={{ float: 'left', margin: 10 }}>
@@ -17,7 +27,10 @@ export default class MachinesList extends Component {
                 <div style={objectStyle}>{machine.name}</div>
                 <div style={objectStyle}>{machine.ip_address}</div>
                 <div style={objectStyle}>{machine.health}</div>
-                <button>Get latest Health</button>
+                <div style={objectStyle}>
+                  <Button name='Get latest Health' clickHandler={() => this.handleClick(machine.id)} />
+                </div>
+
               </div>
             })
           }
